264f9a047f73bb3c346b27de4e62ebad3fa902e4
rook01 <tomasz.gawron@outlook.com>
2025-07-26 18:42:24 +0200

    validator

--- main.js
+++ main.js
@@ -4,8 +4,6 @@ const game = {
   RowKey: "GAME_20250801_LegiaLech",
   GameHome: "Raków Częstochowa",
   GameAway: "Wisła Płock",
-  GameDate: "2025-07-27",
-  GameTime: "14:45",
   PartitionKey: "PL_Ekstraklasa"
 };
 
